6e40cedf7fff00d51323b46a57b36a5c3695566c
Raphael Vigee <contact@raphaelvigee.com>
2018-03-01 02:26:10 +0000

    Add Global env

--- src/main/java/com/raphaelvigee/el/ExpressionLanguage.java
+++ src/main/java/com/raphaelvigee/el/ExpressionLanguage.java
@@ -12,15 +12,18 @@ public class ExpressionLanguage
 
     private Map<String, Function> functions;
 
+    private Map<String, Object> env;
+
     public ExpressionLanguage()
     {
-        this(new HashMap<>());
+        this(new HashMap<>(), new HashMap<>());
     }
 
-    public ExpressionLanguage(Map<String, Function> functions)
+    public ExpressionLanguage(Map<String, Function> functions, Map<String, Object> env)
     {
         this.lexer = new Lexer();
         this.functions = functions;
+        this.env = env;
     }
 
     public <R> R evaluate(String expression)
@@ -41,11 +44,15 @@ public class ExpressionLanguage
         allFunctions.putAll(functions);
         allFunctions.putAll(this.functions);
 
+        Map<String, Object> allEnv = new LinkedHashMap<>();
+        allEnv.putAll(this.env);
+        allEnv.putAll(env);
+
         Parser parser = new Parser(allFunctions);
 
-        Node parsed = parser.parse(stream, env.keySet());
+        Node parsed = parser.parse(stream, allEnv.keySet());
 
-        return (R) parsed.evaluate(env);
+        return (R) parsed.evaluate(allEnv);
     }
 
     public Map<String, Function> getFunctions()
@@ -56,5 +63,15 @@ public class ExpressionLanguage
     public void addFunction(String name, Function function)
     {
         functions.put(name, function);
+    }
+
+    public Map<String, Object> getEnv()
+    {
+        return env;
+    }
+
+    public void add(String name, Object o)
+    {
+        env.put(name, o);
     }
 }
